8d34cdb37bf357142c745ee46de573fae47f7911
sklbz <sokolbozanic@gmail.com>
2025-03-25 22:54:15 +0100

    started the refactory of the player controller

--- Assets/Scripts/Character/CharacterController.cs
+++ Assets/Scripts/Character/CharacterController.cs
@@ -7,14 +7,25 @@ public class CharacterController : MonoBehaviour
 {
     CharacterState characterState;
     Rigidbody2D rigidbody;
+    [SerializeField]
+    LayerMask groundMask;
+    float sqrJumpHeight, jumpForce;
+    Transform[] groundChecks;
 
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        jumpForce = Mathf.Sqrt(-2f * Physics2D.gravity.y * rigidbody.gravityScale);
     }
 
     void Update()
     {
         
+    }
+
+    void VerticalJump() {
+        float velocity = sqrJumpHeight * jumpForce;
+
+        rigidbody.velocity = new Vector2(velocity, 0f);
     }
 }
