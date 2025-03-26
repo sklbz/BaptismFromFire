0780d35277cf12d559be30ade26447f94b232ddb
sklbz <sokolbozanic@gmail.com>
2025-03-26 13:50:59 +0100

    started to add function (grounded and jump)

--- Assets/Scripts/Character/CharacterController.cs
+++ Assets/Scripts/Character/CharacterController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -10,22 +11,38 @@ public class CharacterController : MonoBehaviour
     [SerializeField]
     LayerMask groundMask;
     float sqrJumpHeight, jumpForce;
-    Transform[] groundChecks;
+    List<Transform> groundChecks;
 
     void Start()
     {
+        characterState = GetComponent<CharacterState>();
         rigidbody = GetComponent<Rigidbody2D>();
         jumpForce = Mathf.Sqrt(-2f * Physics2D.gravity.y * rigidbody.gravityScale);
     }
 
     void Update()
     {
-        
+
+        characterState = characterState.handleInput();
     }
 
-    void VerticalJump() {
+    public void VerticalJump() {
         float velocity = sqrJumpHeight * jumpForce;
 
-        rigidbody.velocity = new Vector2(velocity, 0f);
+        rigidbody.velocity = new Vector2(0f, velocity);
+    }
+
+    public bool IsGrounded() {
+
+        bool groundCheckDown = false;
+        foreach (Transform groundCheck in groundChecks)
+        {
+            if (Physics2D.OverlapCircle(groundCheck.position, .1f, groundMask))
+            {
+                groundCheckDown = true;
+                break;
+            }
+        };
+        return groundCheckDown;
     }
 }
